a74bf8d90491ca94c01c9602cae2e74ad2de72ff
Widiyanti004 <105841111621@student.unismuh.ac.id>
2024-05-22 16:46:32 +0800

    Tugas Lab 1

--- App.js
+++ App.js
@@ -1,35 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
 
-export default function App() {
+const App = () =>  {
   return (
-    <View style={styles.container}>
-      <Text style={{
-        color:'black',
-        fontStyle:'italic',
-      }}>Widiyanti love myself </Text>
-      <Text style={{
-        fontSize:20,
-        textDecorationLine:'underline',
-        textShadowColor:'blue',
-        fontStyle: 'italic',
-        color:'brown'}}>Widiyanti love myself
-      </Text><Text style={{
-        fontSize:50,
-        fontStyle:'bold',
-        color:'red'
-
-      }}>Widiyanti love myself 50</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <View style={{ flex: 1, flexDirection: 'row'}}>
+       <View style={{ flex: 1, backgroundColor: 'yellow'}}></View>
+       <View style={{ flex: 1, backgroundColor: 'green'}}></View>
     </View>
-  );
+    
+    <View style={{ flex: 1, flexDirection: 'column'}}>
+      <View style={{ flex: 1, backgroundColor: 'red' }}></View>
+      <View style={{ flex: 1, backgroundColor: 'blue' }}></View>
+    </View>
+  </View>
+    
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});3
+export default App
