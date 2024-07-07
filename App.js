2cb6b90dae0dbae2e46179ba808db544f6718561
Widiyanti004 <105841111621@student.unismuh.ac.id>
2024-07-07 10:48:17 +0800

    Forgot Password

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { View } from 'react-native'
 import React from 'react'
-import  Login from './component/pages/login'
+import  ForgotPassword from './component/pages/forgotPassword'
 
 const App = () => {
   return (
@@ -8,7 +8,7 @@ const App = () => {
     flex: 1,
     backgroundColor: '#EEEEEE'
     }}>
-      <Login />
+      <ForgotPassword/>
 </View>
 )
 }
